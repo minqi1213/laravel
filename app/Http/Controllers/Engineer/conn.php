107562ff062088cc2d61e8425abe5ca3adc6271b
unknown <>
2016-09-26 10:15:31 +0800

    删除engineer文件夹

--- app/Http/Controllers/Engineer/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = @mysql_connect('101.200.179.166','sunnytest','sunnytest123');
-if (!$conn) {
-	die('Could not connect: ' . mysql_error());
-}
-mysql_select_db('zhongbao', $conn);
-
-?>
