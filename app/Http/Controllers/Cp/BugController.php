f6930561986e24877008c8adfa496ed6d7879879
unknown <>
2016-09-26 11:05:16 +0800

    修改欢迎界面内容

---
+++ app/Http/Controllers/Cp/BugController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Cp;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class BugController extends CommonController
+{
+    public function index()
+    {
+        echo 1111;
+    }
+}
