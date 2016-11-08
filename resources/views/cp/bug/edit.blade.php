b3743c7366f04aec4cd714bae938f1c091c656da
unknown <>
2016-11-08 16:55:28 +0800

    修正了ueditor上传图片的大小限制

---
+++ resources/views/cp/bug/edit.blade.php
@@ -0,0 +1,178 @@
+@extends('layouts.cp')
+@section('content')
+    <!--面包屑导航 开始-->
+    <div class="crumb_warp">
+        <!--<i class="fa fa-bell"></i> 欢迎使用登陆网站后台，建站的首选工具。-->
+        <i class="fa fa-home"></i> <a href={{url('engineer/bug')}}>首页</a> &raquo; 新建bug
+    </div>
+    <!--面包屑导航 结束-->
+
+	<!--结果集标题与导航组件 开始-->
+	<div class="result_wrap">
+        <div class="result_title">
+            <h3>快捷操作</h3>
+            @if(count($errors)>0)
+                <div class="mark">
+                    @if(is_object($errors))
+                        @foreach($errors->all() as $error)
+                            <p>{{$error}}</p>
+                        @endforeach
+                    @else
+                        <p>{{$errors}}</p>
+                    @endif
+                </div>
+            @endif
+        </div>
+        <div class="result_content">
+            <div class="short_wrap">
+                <a href="{{url('engineer/bug/create')}}"><i class="fa fa-plus"></i>新建bug</a>
+            </div>
+        </div>
+    </div>
+    <!--结果集标题与导航组件 结束-->
+    
+    <div class="result_wrap">
+        <div id="progress_1" class="fitem" style="padding:5px;">
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(1)">开始流程</a>
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(2)">无效问题/暂不修复</a>
+        </div>
+        <div id="progress_2" class="fitem" style="padding:5px;">
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(0)">停止流程</a>
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(2)">解决问题</a>
+        </div>
+        <div id="progress_3" class="fitem" style="padding:5px;">
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(3)">RBT测试</a>
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(0)">重新打开问题</a>
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(6)">无效问题</a>
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(7)">暂不修复</a>
+        </div>
+        {{--<div id="mm1" style="width:100px;">--}}
+        {{--<div><a onclick="javascript:changeProgress(6)">Invalid Issue</a></div>--}}
+        {{--<div><a onclick="javascript:changeProgress(7)">Don't Fix Now</a></div>--}}
+        {{--</div>--}}
+        <div id="progress_4" class="fitem" style="padding:5px;">
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(4)">回归测试</a>
+        </div>
+        <div id="progress_5" class="fitem" style="padding:5px;">
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(5)">关闭问题</a>
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(0)">重新打开问题</a>
+        </div>
+        <div id="progress_6" class="fitem" style="padding:5px;">
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(0)">重新打开问题</a>
+        </div>
+        <div id="progress_7" class="fitem" style="padding:5px;">
+            <a href="#" class="easyui-linkbutton" data-options="plain:true" onclick="javascript:changeProgress(1)">开始流程</a>
+        </div>
+        <form action="{{url('engineer/bug/'.$field->bid)}}" method="post">
+            <input type="hidden" name="_method" value="put">
+            {{csrf_field()}}
+            <table class="add_tab">
+                <tbody>
+                    <tr>
+                        <th width="120"><i class="require">*</i>项目：</th>
+                        <td>
+                            <select name="pid">
+                                @foreach($data as $d)
+                                    <option value="{{$d->pid}}"
+                                        @if($d->pid==$field->pid) selected
+                                        @endif
+                                    >{{$d->pname}}</option>
+                                @endforeach
+                            </select>
+                        </td>
+                    </tr>
+                    <tr style="display: none">
+                        <th><i class="require">*</i>用户id：</th>
+                        <td>
+                            <input type="text" class="lg" name="uid" value="{{session('userid')}}">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><i class="require">*</i>标题：</th>
+                        <td>
+                            <input type="text" class="lg" name="btitle" value="{{$field->btitle}}">
+                            <p>标题可以写30个字</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>详细内容：</th>
+                        <td>
+                            <script type="text/javascript" charset="utf-8" src="{{asset('resources/org/ueditor/ueditor.config.js')}}"></script>
+                            <script type="text/javascript" charset="utf-8" src="{{asset('resources/org/ueditor/ueditor.all.min.js')}}"> </script>
+                            <!--建议手动加在语言，避免在ie下有时因为加载语言失败导致编辑器加载失败-->
+                            <!--这里加载的语言文件会覆盖你在配置项目里添加的语言类型，比如你在配置项目里配置的是英文，这里加载的中文，那最后就是中文-->
+                            <script type="text/javascript" charset="utf-8" src="{{asset('resources/org/ueditor/lang/zh-cn/zh-cn.js')}}"></script>
+                            <script id="editor" name="bdescription" type="text/plain" style="width:800px;height:500px;">
+                                {!! $field->bdescription !!}
+                            </script>
+                            <script type="text/javascript">
+                                var ue = UE.getEditor('editor');
+                            </script>
+                            <style>
+                                .edui-default{line-height:28px;}
+                                div.edui-combox-body,div.edui-button-body,div.edui-splitbutton-body
+                                {overflow:hidden;height:20px}
+                                div.edui-box{overflow:hidden;height:22px}
+                            </style>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td>
+                            <input type="submit" value="提交">
+                            <input type="button" class="back" onclick="history.go(-1)" value="返回">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    </div>
+    <script>
+        function changeProgress(statusid) {
+            url = "{{url('engineer/changeprogress')}}?_token={{csrf_token()}}";
+            var postStr = "bid=" + {{$field->bid}} + "&status=" + statusid;
+            //alert(postStr);
+            var ajax = false;
+            //开始初始化XMLHttpRequest对象
+            if (window.XMLHttpRequest) { //Mozilla 浏览器
+                ajax = new XMLHttpRequest();
+                if (ajax.overrideMimeType) {//设置MiME类别
+                    ajax.overrideMimeType("text/xml");
+                }
+            }
+            else if (window.ActiveXObject) { // IE浏览器
+                try {
+                    ajax = new ActiveXObject("Msxml2.XMLHTTP");
+                } catch (e) {
+                    try {
+                        ajax = new ActiveXObject("Microsoft.XMLHTTP");
+                    } catch (e) {
+                    }
+                }
+            }
+            if (!ajax) { // 异常，创建对象实例失败
+                window.alert("不能创建XMLHttpRequest对象实例.");
+                return false;
+            }
+            //通过Post方式打开连接
+            ajax.open("POST", url, true);
+            //定义传输的文件HTTP头信息
+            ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            //发送POST数据
+            ajax.send(postStr);
+            //获取执行状态
+            ajax.onreadystatechange = function () {
+                if (ajax.readyState == 4 && ajax.status == 200) {
+                    $('#dg_bug').datagrid('reload');    // reload the bug data
+                    showProgressBar(statusid);
+                    //$('#bug_status_dlg').append(rowformatter_bugstatus(statusid).html());
+                    $("#bug_status_dlg").empty();
+                    $('<div</div>').html((rowformatter_bugstatus(statusid))).appendTo($('#bug_status_dlg'));
+                }
+            }
+        }
+        window.onload=function(){
+            showProgressBar({{$field->status}});
+        }
+    </script>
+@endsection
---
+++ resources/views/cp/bug/index.blade.php
@@ -0,0 +1,172 @@
+@extends('layouts.engineer')
+@section('content')
+    <!--面包屑导航 开始-->
+    <div class="crumb_warp">
+        <!--<i class="fa fa-bell"></i> 欢迎使用登陆网站后台，建站的首选工具。-->
+        <i class="fa fa-home"></i> <a href="{{url('engineer/bug')}}">首页</a> &raquo; 全部bug
+    </div>
+    <!--面包屑导航 结束-->
+    <table width="100%">
+        <tr>
+            <td align="center" valign="middle">
+                <table id="dg_bug" title="我的bug" class="easyui-datagrid" style="width:100%"
+                       url="{{url('engineer/getbug')}}?_token={{csrf_token()}}"
+                       toolbar="#toolbar" pagination="true" pageSize="50" PageList="[50,100,150,200]"
+                       rownumbers="true" fitColumns="true" singleSelect="true" nowrap="false">
+                    <thead>
+                    <tr>
+                        <th width="47%" data-options="field:'btitle',formatter:rowformatter_buglist">标题</th>
+                        <th field="bdescription" hidden="true" width="0%" >详细步骤</th>
+                        <th data-options="field:'status',formatter:rowformatter_bugstatus" width="10%">状态</th>
+                        <th field="btime" width="13%">提交时间</th>
+                        <th data-options="field:'pname'" width="10%">项目名称</th>
+                        <th data-options="field:'username'" width="10%">提交人</th>
+                        <th data-options="field:'bid',formatter:rowformatter_buglist_operate" width="10%">操作</th>
+                    </tr>
+                    </thead>
+                </table>
+                <div id="toolbar" style="padding:5px;height:auto">
+                    <div>
+                        <a href="{{url('engineer/bug/create')}}" class="easyui-linkbutton" iconCls="icon-add" plain="true"
+                           >新建bug</a>
+                    </div>
+                    <div>
+                        &nbsp;&nbsp;&nbsp;&nbsp;请选择项目:&nbsp;&nbsp;
+                        <select id="projectselect_bug" class="easyui-combobox" panelHeight="auto" style="width:100px">
+                            <option value=0>所有项目</option>
+                            @foreach($data as $d)
+                                echo "
+                                <option value="{{$d->pid}}">{{$d->pname}}</option>";
+                            @endforeach
+                        </select>
+                        &nbsp;&nbsp;&nbsp;&nbsp;请选择提交人:&nbsp;&nbsp;
+                        <select id="projectselect_user" class="easyui-combobox" panelHeight="auto" style="width:100px">
+                            <option value=0>所有人</option>
+                            <option value=1>我</option>
+                        </select>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input id="projectselect_input" class="easyui-textbox" style="width:40%"
+                               data-options="prompt:'请输入要搜索的关键字，以空格隔开'"/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a href="#" class="easyui-linkbutton" iconCls="icon-search"
+                           onclick="loadDataGridBugWithParam()">搜索</a>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </table>
+    <!--结果页快捷搜索框 结束-->
+
+    <!--搜索结果页面 列表 开始-->
+    {{--<form action="#" method="post">--}}
+        {{--<div class="result_wrap">--}}
+            {{--<!--快捷导航 开始-->--}}
+            {{--<div class="result_content">--}}
+                {{--<div class="short_wrap">--}}
+                    {{--<a href="{{url('engineer/bug/create')}}"><i class="fa fa-plus"></i>新建bug</a>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<!--快捷导航 结束-->--}}
+        {{--</div>--}}
+
+        {{--<div class="result_wrap">--}}
+            {{--<div class="result_content">--}}
+                {{--<table class="list_tab" border="0" cellspacing="0" cellpadding="0" style="table-layout: fixed;">--}}
+                    {{--<tr>--}}
+                        {{--<th class="tc" width="5%">ID</th>--}}
+                        {{--<th width="47%">标题</th>--}}
+                        {{--<th width="8%">审核状态</th>--}}
+                        {{--<th width="9%">所属项目</th>--}}
+                        {{--<th width="8%">发布人</th>--}}
+                        {{--<th width="13%">更新时间</th>--}}
+                        {{--<th width="15%">操作</th>--}}
+                    {{--</tr>--}}
+                    {{--@foreach($data as $v)--}}
+                    {{--<tr>--}}
+                        {{--<th class="tc">{{$v->bid}}</th>--}}
+                        {{--<td>--}}
+                            {{--<li style="white-space:nowrap;overflow:hidden;text-overflow:ellipsis;-o-text-overflow:ellipsis;-icab-text-overflow: ellipsis; -khtml-text-overflow: ellipsis; -moz-text-overflow: ellipsis; -webkit-text-overflow: ellipsis; ">--}}
+                                {{--<a href="{{url('engineer/bug/'.$v->bid)}}">--}}
+                                    {{--<nobr>{{$v->btitle}}</nobr>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                        {{--</td>--}}
+                        {{--<td style="padding: 0"><select style="width:100%;height:100%" onchange="javascript:location.href=this.value;">--}}
+                                {{--<option value="">全部</option>--}}
+                                {{--<option value="http://www.sina.com">我</option>--}}
+                            {{--</select>--}}
+                        {{--</td>--}}
+                        {{--@if($v->status ==0)--}}
+                            {{--<td class="status"><span style="color:darkblue;">开始</span></td>--}}
+                        {{--@elseif($v->status ==1)--}}
+                            {{--<td class="status"><span style="color:orange;">进行中</span></td>--}}
+                        {{--@elseif($v->status ==2)--}}
+                            {{--<td class="status"><span style="color:orange;">待验证</span></td>--}}
+                        {{--@elseif($v->status ==3)--}}
+                            {{--<td class="status"><span style="color:orange;">RBT</span></td>--}}
+                        {{--@elseif($v->status ==4)--}}
+                            {{--<td class="status"><span style="color:orange;">待回归</span></td>--}}
+                        {{--@elseif($v->status ==5)--}}
+                            {{--<td class="status"><span style="color:forestgreen;">关闭</span></td>--}}
+                        {{--@elseif($v->status ==6)--}}
+                            {{--<td class="status"><span style="color:forestgreen;">无效问题</span></td>--}}
+                        {{--@elseif($v->status ==7)--}}
+                            {{--<td class="status">'<span style="color:forestgreen;">暂不修复</span></td>--}}
+                        {{--@else--}}
+                            {{--<td class="status"><span style="color:darkblue;">未知状态</span></td>--}}
+                        {{--@endif--}}
+                        {{--<td class="pname">{{$v->pname}}</td>--}}
+                        {{--<td class="username">{{$v->username}}</td>--}}
+                        {{--<td style="white-space:nowrap;overflow:hidden;text-overflow:ellipsis;-o-text-overflow:ellipsis;-icab-text-overflow: ellipsis; -khtml-text-overflow: ellipsis; -moz-text-overflow: ellipsis; -webkit-text-overflow: ellipsis; ">{{$v->btime}}</td>--}}
+                        {{--<td>--}}
+                            {{--<a href="{{url('engineer/bug/'.$v->bid.'/edit')}}">修改</a>--}}
+                            {{--<a href="#">删除</a>--}}
+                        {{--</td>--}}
+                    {{--</tr>--}}
+                    {{--@endforeach--}}
+                {{--</table>--}}
+
+                {{--<div class="page_list">--}}
+                    {{--<ul>--}}
+                        {{--{{$data->links()}}--}}
+                    {{--</ul>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</form>--}}
+    <!--搜索结果页面 列表 结束-->
+    {{--<style>--}}
+        {{--.result_content ul li span{--}}
+            {{--font-size: 15px;--}}
+            {{--padding: 6px 12px;--}}
+        {{--}--}}
+    {{--</style>--}}
+    {{--<script type="text/javascript">--}}
+        {{--$(function(){--}}
+
+            {{--$('.pid').click(function(){--}}
+                {{--if(!$(this).is('.input')){--}}
+                    {{--$(this).addClass('input').html('<input style="width:90%" type="text" value="'+ $(this).text() +'" />').find('input').focus().blur(function(){--}}
+                        {{--var thisid = $(this).parent().siblings("th:eq(0)").text();--}}
+                        {{--var thisvalue=$(this).val();--}}
+                        {{--var thisclass = $(this).parent().attr("class");--}}
+                        {{--alert(thisid);--}}
+                        {{--alert(thisvalue);--}}
+                        {{--alert(thisclass);--}}
+{{--//                        $.ajax({--}}
+{{--//                            type: 'POST',--}}
+{{--//                            url: 'update.php',--}}
+{{--//                            data: "thisid="+thisid+"&thisclass="+thisclass+"&thisvalue="+thisvalue--}}
+{{--//                        });--}}
+                        {{--$(this).parent().removeClass('input').html('<li><a href=\"http://www.baidu.com\">'+$(this).val() || 0+'</a></li>');--}}
+                    {{--});--}}
+                {{--}--}}
+            {{--}).hover(function(){--}}
+                {{--$(this).addClass('hover');--}}
+            {{--},function(){--}}
+                {{--$(this).removeClass('hover');--}}
+            {{--});--}}
+
+        {{--});--}}
+    {{--</script>--}}
+@endsection
